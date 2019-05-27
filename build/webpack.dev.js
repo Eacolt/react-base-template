fe5db9d41252b640d8176f44154a2cd4e72a38ce
liuzhaoting <liuzhaoting@100tal.com>
2019-05-27 16:24:52 +0800

    ok

--- build/webpack.dev.js
+++ build/webpack.dev.js
@@ -1,31 +1,38 @@
 const merge = require('webpack-merge')
 const webpackConfig = require('./webpack.config')
+const portfinder = require('portfinder')
+const path = require('path')
 
- 
-module.exports = merge(webpackConfig,{
-    mode:'development',
-    devtool:'cheap-module-eval-source-map',
-    module:{
-        rules:[
-            {
-                
-            test:/\.(css|less)$/,
-            use:[{
-                loader:'style-loader'
-               
-            },'css-loader','less-loader']
-                
-            }
-        ]
-    },
- 
-    devServer:{
-        host:'localhost',
-        hot:true,
-        port:'8083',
-        historyApiFallback:true,
-        open:true,
-        contentBase:'./dist'
-
-    }
+portfinder.basePort = 8080;
+module.exports = portfinder.getPortPromise().then(port=>{
+    return merge(webpackConfig,{
+        mode:'development',
+        devtool:'cheap-module-eval-source-map',
+        module:{
+            rules:[
+                {
+                    
+                test:/\.(css|less)$/,
+                use:[{
+                    loader:'style-loader'
+                   
+                },'css-loader','less-loader']
+                    
+                }
+            ]
+        },
+     
+        devServer:{
+            host:'localhost',
+            headers:{
+                "Access-Control-Allow-Origin":"*"
+            },
+            hot:true,
+            port:port,
+            historyApiFallback:true,
+            open:true,
+            contentBase:path.resolve(__dirname,'dist')
+    
+        }
+    })
 })
